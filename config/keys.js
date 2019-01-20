67627e94670973ffb0b138d87b07c7e709e3ca57
shamnas cv <shamnas@socialhoox.com>
2019-01-20 14:04:23 +0530

    gitignore changes

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID : 'your-google-client-id',
+    googleClientSecret: 'your-google-client-secret',
+    mongooseURI: 'mongodb://[username]:[password]@ds161950.mlab.com:61950/emaily-dev',
+    cookieKey: 'any-random-key'
+}
